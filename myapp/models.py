0a17ff1d1f25ec7b1ac529753a8d25d04a95d7d3
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2024-01-11 20:19:57 +0530

    added resume and project model to backend

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Resume(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    resume_file = models.FileField(upload_to='resumes/')
+
+    def __str__(self):
+        return self.title
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='project_images/', blank=True, null=True)
+   
+
+    def __str__(self):
+        return self.title
+    
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Resume,Project
 # Create your views here.
+def home(request):
+    resume = Resume.objects.get(pk=1)
+    return render(request,'myapp/home.html',{'resume':resume})
